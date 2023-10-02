3a3b5b96f5b58f7fe8396802cb6da5e1d096aa2b
Shafreed <shafsshafreedh321@gmail.com>
2023-10-02 12:46:13 +0530

    first commit

---
+++ src/ReducerComponent.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react'
+
+const initialState=0
+const reducer=(state=initialState,action)=>{
+    switch(action){
+        case "increment":
+        return state=state+1
+        case "decrement":
+        return state=state-1
+        case "reset":
+        return state=0
+
+        default :
+        return state
+    }
+}
+
+const ReducerComponent = () => {
+    
+const [count,dispatch]=useReducer(reducer,initialState)
+ 
+return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=>dispatch('increment')}>Increment</button>
+        <button onClick={()=>dispatch('decrement')}>Decrement</button>
+        <button onClick={()=>dispatch('reset')}>Reset</button>
+    </div>
+  )
+}
+
+export default ReducerComponent
